3f61ac3c7d7cf3436bb18fde44e0551624560248
Thinh <nthinh92@gmail.com>
2020-09-12 10:50:07 +0700

    thinh upgit lan 1 ngay thu 7

--- src/main/java/com/casestudy/repository/UserRepository.java
+++ src/main/java/com/casestudy/repository/UserRepository.java
@@ -1,10 +1,16 @@
 package com.casestudy.repository;
 
+import com.casestudy.model.Role;
 import com.casestudy.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     User findByName(String username);
+
+    Iterable<User> findAllByRole(Role role);
+
 }
--- src/main/java/com/casestudy/service/user/UserServiceImpl.java
+++ src/main/java/com/casestudy/service/user/UserServiceImpl.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService, UserDetailsService {
@@ -33,11 +32,9 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     }
 
     @Override
-    public Optional<User> findById(Long id) {
-        return userRepository.findById(id);
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(null);
     }
-
-
 
     @Override
     public void save(User user) {
